ae25a5d47a336c82ea214ecbc591d7e83407e107
Gloire__ODG <93401301+ondongo@users.noreply.github.com>
2024-03-05 07:01:29 +0000

    integration analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Head from "next/head";
 import React from "react";
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,6 +22,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <Providers>{children}</Providers>
+        <Analytics/>
       </body>
       <script
         src="//code.tidio.co/ctthkrbemuzwkzdtga5zcn6hwozf1vfs.js"
